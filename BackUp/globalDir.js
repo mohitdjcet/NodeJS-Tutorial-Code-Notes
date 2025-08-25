0ceb2e61e1c5e920d4d992890c4b5f06b43f475a
Mohit Kumar <mks.djcet@gmail.com>
2025-08-25 14:15:23 +0530

    updated code

---
+++ BackUp/globalDir.js
@@ -0,0 +1,14 @@
+console.log(__dirname);
+console.log(__filename);
+
+const fs = require('fs');
+
+console.log(process.cwd());
+console.log(process.pid);
+console.log(process.platform);
+console.log(process.version);
+
+console.log(process.env.USER);
+
+console.log(process.exit());
+
