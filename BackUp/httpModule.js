0db20c4255ea7f154315267ae7e9015b072ae08a
Mohit Kumar <mks.djcet@gmail.com>
2025-08-26 20:01:37 +0530

    Updated code

--- BackUp/httpModule.js
+++ BackUp/httpModule.js
@@ -26,7 +26,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
         res.writeHead(200,{'Content-Type':'text/plain'});
-        res.write(JSON.stringify({name: "Mohit", age: 24}));
+        res.write(JSON.stringify({name: "Mohit", age: 24, city: "Delhi"}));
         res.end();
     } else {
         res.writeHead(404,{'Content-Type':'text/html'});
